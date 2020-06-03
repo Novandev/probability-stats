1e0f51ce6bca5e03a0c4470fa898e5ddbba1645f
novandev <novanadams35@gmail.com>
2020-06-03 15:42:02 -0700

    add/ more

--- probability/additive.py
+++ probability/additive.py
@@ -4,3 +4,15 @@ Tha additive low of probability
 This law gives you the sum probabilities of both independent depended event that may be exclusive or mutually exlusive
 
 """
+
+
+
+
+
+def additive_probability_dependent(a=0,b=0, pr):
+    """
+    This aasumes the prob is nto complemented
+    
+    """
+    if a == 0 or b == 0 :
+        assert (a ==0 or b == 0), 
